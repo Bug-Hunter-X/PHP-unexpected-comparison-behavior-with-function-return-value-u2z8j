8da2e0b5b76ea7c39c682441f52523b034306fc1
BugHunter-X <askzenai@gmail.com>
2024-12-10 02:28:04 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,13 @@
+function myFunc() {
+  //Some code here...
+  return $someVariable;
+}
+
+// Calling the function
+$result = myFunc();
+
+//Further use of the result
+if ($result === true) { //Problem occurs here
+    //Some code to be executed when the result is true
+}
+
